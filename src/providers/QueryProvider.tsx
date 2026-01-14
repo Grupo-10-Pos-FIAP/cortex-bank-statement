e912419952b23cf18558fa6e4bedac33739ab9e5
Gabrielle-96 <gabrielle.martiins.96@gmail.com>
2026-01-14 19:51:26 -0300

    chore(components): remove ReactQueryDevtools from QueryProvider

--- src/providers/QueryProvider.tsx
+++ src/providers/QueryProvider.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { getQueryClient } from "@/lib/queryClient";
 
 interface QueryProviderProps {
@@ -13,7 +12,6 @@ export function QueryProvider({ children }: QueryProviderProps) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      {process.env.NODE_ENV === "development" && <ReactQueryDevtools initialIsOpen={false} />}
     </QueryClientProvider>
   );
 }
